3c3c7d6d6a67241cf4ff5f2f34ed4392fce3124b
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-05 14:35:18 +0100

    feat: even_odd ex completed

--- JS/evenodd.js
+++ JS/evenodd.js
@@ -1,22 +1,26 @@
 // Utente sceglie pari o dispari
-const direzione = prompt("Scegli pari o dispari?");
+const direzionePlayer = prompt("Scegli paro o disparo?");
+
+// In base alla direzione scelta dall'utente scelgo la direzione del pc
+const direzionePC = direzionePlayer === "paro" ? "disparo" : "paro";
 
 // Utente inserisce un numero
 const puntata = parseInt(prompt("Che numero punti?"));
 
+const numPC = pcNumber(1, 5);
+
 // Sommiamo il numero random del pc con quello inserito dall'utente
-const somma = puntata + pcNumber(1, 5);
+const somma = puntata + numPC;
 
 // Questa somma è pari o dispari?
 const resultSomma = even_odd(somma);
 
-// console.log(somma);
-// console.log(resultSomma);
+let finalMsg = "";
 
-const finalMsg = "";
-
-if (resultSomma === "pari" && direzione === "pari")
-  finalMsg = "Ha vinto l'utente";
+if (resultSomma === direzionePlayer)
+  finalMsg = `Ha vinto l'utente inserendo ${puntata} e scegliendo ${direzionePlayer}, il pc ha inserito ${numPC} e puntato sul numero ${direzionePC}, la somma finale è uguale a ${somma} quindi il numero finale è ${resultSomma}.`;
+else if (resultSomma === direzionePC)
+  finalMsg = `Ha vinto il pc inserendo ${numPC} e scegliendo ${direzionePC}, l'utente ha inserito ${puntata} e puntato sul numero ${direzionePlayer}, la somma finale è uguale a ${somma} quindi il numero finale è ${resultSomma}.`;
 
 console.log(finalMsg);
 
