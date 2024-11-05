0699df0c8e9e2739c49851f04c5920713f8d94d0
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-05 13:56:40 +0100

    feat:First exercise completed

--- JS/pali.js
+++ JS/pali.js
@@ -1,12 +1,34 @@
 // Input utente
 const word = prompt("Inserisci una parola");
 
+// Richiamo la funzione che controlla se la parola è palindroma
+
+const resultPalindrome = isPalidrome(word);
+
+const stringResult = resultPalindrome
+  ? `La parola ${word} è Palindroma`
+  : `La parola ${word} non è Palindroma`;
+
+console.log(stringResult);
 
 /**
  * Check if the word is palindrome
  * @param {string} word
  * @returns {boolean}
  */
-function isPalidrome(word){
+function isPalidrome(word) {
+  let newWord = word[word.length - 1];
 
+  //   console.log(newWord);
+
+  // Devo iterare la stringa dalla fine verso l'inizio
+  for (let i = word.length - 1; i >= 0; i--) {
+    // Carattere corrente della parola
+    let currCar = word[i];
+    if (i != word.length - 1) newWord += currCar;
+  }
+
+  //   console.log(newWord);
+
+  return word === newWord;
 }
