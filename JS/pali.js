61707db7b4e5d8f41dfb2b608aa11b98782c2e06
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-05 13:25:57 +0100

    first commit

---
+++ JS/pali.js
@@ -0,0 +1,12 @@
+// Input utente
+const word = prompt("Inserisci una parola");
+
+
+/**
+ * Check if the word is palindrome
+ * @param {string} word
+ * @returns {boolean}
+ */
+function isPalidrome(word){
+
+}
